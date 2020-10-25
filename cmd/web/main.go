9aad1a6beec688d7fdbb644f369c3e843cfaf60e
nicko <nicko.lee@hotmail.com>
2020-10-25 11:28:23 +0000

    Add ability to serve static files
    - Create fileServer handler and give it a directory on the server file system to bind it to
    - Register a new URL pattern to handle incoming requests to "/static/" and map it to its handler (the fileserver we just created)
    - Before we hand it over strip the "/static" prefix

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,6 +13,12 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// Create a fileserver which serves files out of the "./ui/static" directory.
+	// Register the file server as the handler for all URL paths that start with "/static/"
+	// For matching paths, strip the "/static" prefix before the request reaches the file server
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	// Spin up a new web server and pass it the servemux (router) we just created + a port number
 	// If http.ListenAndServe() returns an error we use the log.Fatal() function to log the error message and exit.
 	// Note that any error returned by http.ListenAndServe() is always non-nil.
